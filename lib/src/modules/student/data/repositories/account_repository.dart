bcd4930f21079ec478c8b1e7b5d316b0984e3bca
Mahmoud Atef <mahmoud.atef3167@gmail.com>
2025-07-28 23:07:30 +0300

    feat: add student remote services and repository for profile management

---
+++ lib/src/modules/student/data/repositories/account_repository.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:sunnah_academy/src/modules/student/data/models/student.dart';
+import 'package:sunnah_academy/src/modules/student/data/services/account_remote_services.dart';
+
+class StudentRepository {
+  final StudentRemoteServices _remoteServices;
+  const StudentRepository(this._remoteServices);
+
+  Future<Either<Exception, Student>> getProfile() {
+    return _remoteServices.getProfile();
+  }
+
+  Future<Either<Exception, Student>> updateProfile({
+    String? name,
+    String? phoneNumber,
+    String? gender,
+    String? birthDate,
+  }) {
+    return _remoteServices.updateProfile(
+        gender: gender,
+        name: name,
+        birthDate: birthDate,
+        phoneNumber: phoneNumber);
+  }
+
+  Future<Either<Exception, Unit>> deleteAccount() {
+    return _remoteServices.deleteAccount();
+  }
+}
---
+++ lib/src/modules/student/data/services/account_remote_services.dart
@@ -0,0 +1,54 @@
+import 'package:dartz/dartz.dart';
+import 'package:sunnah_academy/src/core/apis/end_points.dart';
+import 'package:sunnah_academy/src/modules/student/data/models/student.dart';
+
+import '../../../../core/apis/dio_helper.dart';
+
+class StudentRemoteServices {
+  /// Get student profile
+  Future<Either<Exception, Student>> getProfile() async {
+    try {
+      var response = await DioHelper.getData(path: EndPoints.profile);
+      var student = Student.fromJson(response.data['data']);
+      return Right(student);
+    } on Exception catch (e) {
+      return Left(e);
+    }
+  }
+
+  /// Update student profile
+  Future<Either<Exception, Student>> updateProfile({
+    String? name,
+    String? phoneNumber,
+    String? gender,
+    String? birthDate,
+  }) async {
+    try {
+      // Prepare data map, only include non-null values
+      Map<String, dynamic> data = {};
+      if (name != null) data['name'] = name;
+      if (phoneNumber != null) data['phoneNumber'] = phoneNumber;
+      if (gender != null) data['gender'] = gender;
+      if (birthDate != null) data['birthDate'] = birthDate;
+
+      var response = await DioHelper.putData(
+        path: EndPoints.profile,
+        data: data,
+      );
+      var student = Student.fromJson(response.data['data']);
+      return Right(student);
+    } on Exception catch (e) {
+      return Left(e);
+    }
+  }
+
+  /// Delete student student
+  Future<Either<Exception, Unit>> deleteAccount() async {
+    try {
+      await DioHelper.deleteData(path: EndPoints.profile);
+      return const Right(unit);
+    } on Exception catch (e) {
+      return Left(e);
+    }
+  }
+}
